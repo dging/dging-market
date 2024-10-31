3e7bc815d37edbe05318799952f41d5d832847dc
tenacy <tentenacy@gmail.com>
2024-10-31 18:59:25 +0900

    chore: 알림 관련 DTO 추가

---
+++ backend/src/main/java/com/dging/dgingmarket/web/api/dto/notification/NotificationsResponse.java
@@ -0,0 +1,21 @@
+package com.dging.dgingmarket.web.api.dto.notification;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *  채팅 내역 조회 응답 DTO
+ */
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class NotificationsResponse {
+
+    private Long id;
+    private Long relatedId;
+    private String title;
+    private String content;
+    private String thumbnailUrl;
+}
