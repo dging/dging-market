db2bc4fbdf374fdcbfcb8ad7d95e233ef8585175
tenacy <tentenacy@gmail.com>
2024-11-08 11:13:21 +0900

    test: 테스트 페이지 채팅방별 메시지 표시

--- backend/src/main/java/com/dging/dgingmarket/web/controller/ChatController.java
+++ backend/src/main/java/com/dging/dgingmarket/web/controller/ChatController.java
@@ -1,20 +1,30 @@
 package com.dging.dgingmarket.web.controller;
 
 import com.dging.dgingmarket.dto.ChatMessageDto;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat/send")
-    @SendTo("/topic/chat")
-    public ChatMessageDto sendMessage(@Payload ChatMessageDto message, SimpMessageHeaderAccessor headerAccessor) {
+    @MessageMapping("/chat/{chatRoomId}/send")
+    @SendTo("/topic/chat/{chatRoomId}")
+    public ChatMessageDto sendMessage(
+            @DestinationVariable Long chatRoomId,
+            @Payload ChatMessageDto message,
+            SimpMessageHeaderAccessor headerAccessor
+    ) {
+
         String sessionId = headerAccessor.getSessionId(); // 세션 ID 가져오기
         System.out.println("Message received from session ID: " + sessionId);
+
+        message.setChatRoomId(chatRoomId);
+
         return message;
     }
 }
